4e2c9150b8915e250c51756a2f12dc11434a9e95
zkldi <ddhitone@gmail.com>
2020-05-05 19:35:59 +0100

    get online users sanely

--- api/v1/users/userID/friends/friends.js
+++ api/v1/users/userID/friends/friends.js
@@ -4,16 +4,20 @@ const router = express.Router({mergeParams: true});
 const userHelpers = require("../../../../../helpers/userhelpers.js");
 const middlewares = require("../../../../../middlewares.js");
 const friendHelpers = require("./helpers.js");
+const apiConfig = require("../../../../../apiconfig.js");
 
 // mounted on /api/v1/users/:userID/friends
 
 router.get("/", async function(req,res){
     let user = await userHelpers.GetUser(req.params.userID);
+    
+    let friends = await db.get("users").find({id: {$in: user.friends}}, {fields: apiConfig.REMOVE_PRIVATE_USER_RETURNS});
+
     return res.status(200).json({
         success: true,
         description: "Successfully found friends of user '" + req.params.userID + "'",
         body: {
-            friendIDs: user.friends
+            items: friends
         }
     });
 });
@@ -21,17 +25,15 @@ router.get("/", async function(req,res){
 router.get("/online", async function(req,res){
     let user = await userHelpers.GetUser(req.params.userID);
 
-    let onlineStatum = user.friends.map(f => userHelpers.IsUserIDOnline(f));
-
-    onlineStatum = await Promise.all(onlineStatum);
-
-    let onlineFriends = user.friends.filter((f,i) => onlineStatum[i])
+    let friends = await db.get("users").find({id: {$in: user.friends}, lastSeen: {
+        $gt: curTime - apiConfig.TIME_DELTA_ONLINE
+    }}, {fields: apiConfig.REMOVE_PRIVATE_USER_RETURNS});
 
     return res.status(200).json({
         success: true,
         description: "Successfully found friends of user '" + req.params.userID + "'",
         body: {
-            friendIDs: onlineFriends
+            items: friends
         }
     });
 });
@@ -40,11 +42,28 @@ router.patch("/add", middlewares.RequireUserKeyMatch, async function(req,res){
     let friend = await friendHelpers.GetFriend(req.body.friendID);
     let user = await userHelpers.GetUser(req.params.userID);
 
+    if (user.friends.length > 100){
+        return res.status(400).json({
+            success: false,
+            description: "You have reached the friend limit."
+        });
+    }
+
+    if (user.friends.includes(friend.id)){
+        return res.status(400).json({
+            success: false,
+            description: "You are already friends with this user."
+        })
+    }
+
     await db.get("users").update({_id: user._id}, {$push: {friends: friend.id}});
     
     return res.status(201).json({
         success: true,
-        description: "Successfully added friendID '" + req.body.friendID + "' to user '" + req.params.userID + "'"
+        description: "Successfully added friendID '" + req.body.friendID + "' to user '" + req.params.userID + "'",
+        body: {
+            item: friend
+        }
     });
 });
 
@@ -65,7 +84,10 @@ router.patch("/remove", middlewares.RequireUserKeyMatch, async function(req,res)
     
     return res.status(200).json({
         success: true,
-        description: "Successfully removed friendID '" + req.body.friendID + "' from user '" + req.params.userID + "'"
+        description: "Successfully removed friendID '" + req.body.friendID + "' from user '" + req.params.userID + "'",
+        body: {
+            item: friend
+        }
     });
 });
 
