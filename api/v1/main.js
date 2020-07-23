2c52d20dc070c1a69e922204f6640b4d2c98a17a
zkldi <ddhitone@gmail.com>
2020-07-23 21:10:47 +0100

    v1.1.2

--- api/v1/main.js
+++ api/v1/main.js
@@ -4,8 +4,8 @@ const router = express.Router({mergeParams: true});
 // mounted on /v1
 
 const MAJOR_VER = 1;
-const MINOR_VER = 0;
-const PATCH_VER = 0;
+const MINOR_VER = 1;
+const PATCH_VER = 2;
 
 router.get("/", async function(req,res){
     return res.status(200).json({
