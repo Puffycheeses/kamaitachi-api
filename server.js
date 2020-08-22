66172cc7861fd12537e47f33444f5d4038c68ea9
zkldi <ddhitone@gmail.com>
2020-08-22 06:56:21 +0100

    more staging => prod fixes

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const cookieParser = require('cookie-parser');
 const app = express();
 app.set("trust proxy", 1);
 
+process.env.NODE_ENV = process.env.NODE_ENV || "production";
+
 // allow cors requests from kamaitachi.xyz
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://kamaitachi.xyz");
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", process.env.NODE_ENV === "production" ? "https://kamaitachi.xyz" : "http://127.0.0.1:8080");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET,POST,PATCH,PUT,DELETE,OPTIONS");
@@ -19,7 +21,10 @@ app.use(function(req, res, next) {
 // in order to send CORS requests with methods other than GET or POST
 // an OPTIONS request to the same endpoint must return EXACTLY
 // an empty 200 response.
-app.use(function(req,res, next){;
+
+// since we don't use OPTIONS for anything, we can just hack around this
+// and always return an empty 200 - even if that request is completely invalid.
+app.use((req,res, next) => {;
     if (req.method === "OPTIONS"){
         res.header("Access-Control-Max-Age", 60 * 60 * 24 * 365);
         return res.status(200).send();
@@ -56,7 +61,7 @@ db.then(() => {
 });
 
 // mounts
-const apiRouterV1 = require("./api/v1/main.js");    
+const apiRouterV1 = require("./api/v1/main.js");
 app.use("/v1", apiRouterV1)
 
 // if anything has not been found by this point, they're 404ing.
