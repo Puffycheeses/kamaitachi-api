fb61ee22fbe5b015b5551b34ad2a7b39e1f0be5e
zkldi <ddhitone@gmail.com>
2020-08-25 09:35:27 +0100

    fixed typo

--- api/v1/scores/scoreID/scoreID.js
+++ api/v1/scores/scoreID/scoreID.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router({mergeParams: true});
-const db = require("../../../db.js");
+const db = require("../../../../db.js");
 
 // mounted on /api/v1/scores/:scoreID
 
