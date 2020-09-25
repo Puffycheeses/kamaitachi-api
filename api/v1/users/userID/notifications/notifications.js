416fd9d789fba0319d47fb519d5533c394768e88
zkldi <ddhitone@gmail.com>
2020-09-26 00:58:11 +0100

    delete notifications

--- api/v1/users/userID/notifications/notifications.js
+++ api/v1/users/userID/notifications/notifications.js
@@ -3,6 +3,7 @@ const router = express.Router({mergeParams: true});
 const userHelpers = require("../../../../../core/user-core.js");
 const middlewares = require("../../../../../middlewares.js");
 const dbCore = require("../../../../../core/db-core.js");
+const db = require("../../../../../db.js");
 
 // mounted on /api/v1/users/:userID/notifications
 router.use(middlewares.RequireUserKeyMatch);
@@ -37,6 +38,30 @@ router.get("/", async function(req,res){
             });
         }
     }
+});
+
+router.delete("/delete/:notifID", async function (req,res){
+    let notif = await db.get("notifications").findOne({
+        notifID: req.params.notifID,
+        toUserID: req.apikey.assignedTo
+    });
+
+    if (!notif){
+        return res.status(404).json({
+            success: false,
+            description: "This notification does not exist or no longer exists."
+        });
+    }
+
+    await db.get("notifications").remove({
+        _id: notif._id
+    });
+
+    return res.status(200).json({
+        success: true,
+        description: "Successfully deleted notification.",
+        body: notif
+    })
 });
 
 // sugar for notifications?read=false
