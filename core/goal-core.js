0ee8981b780a1bcef344f6f2910121d99517f619
zkldi <ddhitone@gmail.com>
2020-09-27 19:52:50 +0100

    more goal-core fixes

--- core/goal-core.js
+++ core/goal-core.js
@@ -1,4 +1,5 @@
 const db = require("../db.js");
+const config = require("../config/config.js");
 
 async function EvaluateGoalForUser(goalID, userID){
     let goalObj = await db.get("goals").findOne({
@@ -19,21 +20,33 @@ async function EvaluateGoalForUser(goalID, userID){
 
     let formattedScoreQuery = ConvertKeynames(goalObj.scoreQuery);
 
+    let baseObj = {
+        userID: userID,
+        game: goalObj.game,
+        "scoreData.playtype": goalObj.playtype
+    }
+
+    // if null, we are looking for the set of all charts.
+    if (chartIDs){
+        baseObj.chartID = {$in: chartIDs}
+    }
+
     let result = await db.get("scores")[method](
-        Object.assign({
-            chartID: {$in: chartIDs},
-            userID: userID
-        },
+        Object.assign(
+            baseObj,
             formattedScoreQuery
         )
     );
 
     if (goalObj.criteria.type === "anyMatch"){
-        let key = Object.keys(formattedScoreQuery)[0];
+        let keys = Object.keys(formattedScoreQuery);
 
+        // do some hackery to set "r" as the "result" of the query,
+        // since the above query is only checking count against count (i.e. 5 hcs against 10 hcs)
+        // when we want something like: score 740 against score 800
         let pb;
         let r = 0;
-        if (key === "scoreData.lampIndex"){
+        if (keys.includes("scoreData.lampIndex")){
             pb = await db.get("scores").findOne({
                 chartID: {$in: chartIDs},
                 userID: userID,
@@ -49,37 +62,41 @@ async function EvaluateGoalForUser(goalID, userID){
         }
 
         if (pb) {
-            let keys = key.split(".");
+            let keyxs = keys[0].split(".");
     
             r = pb;
-            for (const k of keys) {
+            for (const k of keyxs) {
                 r = r[k];
             }
         }
 
-        return {result: r, success: !!result};
+        // BIG RED HACK WARNING AAA
+        let outOf = (formattedScoreQuery[keys[0]] && formattedScoreQuery[keys[0]]["$gte"]) ? formattedScoreQuery[keys[0]]["$gte"] : null;
+        return {result: r, outOf: outOf, success: !!result, goalObj};
     }
     
     let value = goalObj.criteria.value;
 
     if (goalObj.criteria.type === "gt"){
-        return {result, success: result > value};
+        return {result, success: result > value, goalObj};
+    }
+    else if (goalObj.criteria.type === "all" || value === -1){ // legacy
+        return {result, outOf: chartIDs.length, success: result > chartIDs.length, goalObj}
     }
     else if (goalObj.criteria.type === "gte"){
-        return {result, success: result >= value};
+        return {result, outOf: value, success: result >= value, goalObj};
     }
-    // these don't really make sense for goals, so they're just commented out.
-    // else if (goalObj.criteria.type === "lt"){
-    //     return result < value;
-    // }
-    // else if (goalObj.criteria.type === "lte"){
-    //     return result <= value;
-    // }
+    else if (goalObj.criteria.type === "lt"){
+        return {result, outOf: value, success: result < value, goalObj};
+    }
+    else if (goalObj.criteria.type === "lte"){
+        return {result, outOf: value, success: result <= value, goalObj};
+    }
     else if (goalObj.criteria.type === "eq"){
-        return {result, success: result === value};
+        return {result, outOf: value, success: result === value, goalObj};
     }
     else {
-        return {result}; // ????
+        return {result, outOf: value, success: false, goalObj}; // ???? failsafe
     }
 }
 
@@ -104,10 +121,25 @@ async function GetChartIDsFromGoal(goalObj){
         return [goalObj.directChartID];
     }
 
+    if (goalObj.directChartIDs) {
+        return goalObj.directChartIDs;
+    }
+
+    // if null, we don't care about any subset of charts
+    if (!goalObj.chartQuery){
+        return null;
+    }
+
     let formattedChartQuery = ConvertKeynames(goalObj.chartQuery.query);
 
     let charts = await db.get(goalObj.chartQuery.collection).find(formattedChartQuery);
-    return charts.map(e => e.chartID);
+
+    let chartIDs = charts.map(e => e.chartID);
+
+    if (goalObj.chartQuery.collection === "tierlistdata" && goalObj.game === "iidx"){
+        chartIDs = charts.filter(e => e.flags && e.flags["IN BASE GAME"]).map(e => e.chartID); // hack fix for iidx
+    }
+    return chartIDs
 }
 
 async function CreateUserGoal(goal, userID){
@@ -121,13 +153,41 @@ async function CreateUserGoal(goal, userID){
         timeAchieved: null,
         note: null,
         progress: null,
-        progressHuman: null
+        progressHuman: null,
+        outOf: null,
+        outOfHuman: null
     }
 
     let goalStatus = await EvaluateGoalForUser(goal.goalID, userID);
 
+    let progressHuman = goalStatus.result;
+    let outOfHuman = goalStatus.outOf;
+
+    let goalObj = goalStatus.goalObj;
+
+    if (goalObj.criteria.type === "anyMatch"){
+        let keys = Object.keys(goal.scoreQuery);
+
+        if (keys.includes("scoreData¬percent")){
+            progressHuman = goalStatus.result.toFixed(2) + "%";
+            outOfHuman = goalStatus.outOf.toFixed(2) + "%";
+        }
+        else if (keys.includes("scoreData¬gradeIndex")){
+            progressHuman = config.grades[goalObj.game][goalStatus.result];
+            outOfHuman = config.grades[goalObj.game][goalStatus.outOf];
+        }
+        else if (keys.includes("scoreData¬lampIndex")){
+            progressHuman = config.lamps[goalObj.game][goalStatus.result];
+            outOfHuman = config.lamps[goalObj.game][goalStatus.outOf];
+        }
+    }
+
     ugObj.achieved = goalStatus.success;
     ugObj.progress = goalStatus.result;
+    ugObj.progressHuman = progressHuman;
+    ugObj.outOfHuman = outOfHuman;
+    ugObj.outOf = goalStatus.outOf;
+
     await db.get("user-goals").insert(ugObj);
     
     return ugObj;
