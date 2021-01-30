18b96dd460288aa11fbad853dce5527c52892c6e
zkldi <ddhitone@gmail.com>
2021-01-30 16:04:26 +0000

    Remove self explanatory comments

--- @types/express-serve-static-core/index.d.ts
+++ @types/express-serve-static-core/index.d.ts
@@ -4,8 +4,6 @@ export {};
 declare global {
     namespace Express {
         interface Request {
-            // query: Record<string, string>;
-            // body: Record<string, string>;
             apikey: PublicAPIKeyDocument | null;
             /**
              * Refers to the user that is making the request. For guested requests, this is null.
@@ -15,18 +13,10 @@ declare global {
              * For requests that involve a userID in params, requestedUser contains said requested user.
              */
             requestedUser: PublicUserDocument | null;
-            /**
-             * For scoreID endpoints, the specific score requested by the user.
-             */
             score?: ScoreDocument;
-            /**
-             * For folder related endpoints, this is the folder requested
-             */
             folderData?: FolderDocument;
-            /**
-             * For rival related endpoints, this is the folder requested
-             */
             rivalGroup?: RivalGroupDocument;
+            song?: SongDocument;
         }
     }
 }
