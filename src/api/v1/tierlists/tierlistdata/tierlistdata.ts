a6cc675316e13b133e8a351d653cb53873913434
zkldi <ddhitone@gmail.com>
2021-01-30 12:41:27 +0000

    Completely refactor tierlistdata.ts

--- src/api/v1/tierlists/tierlistdata/tierlistdata.ts
+++ src/api/v1/tierlists/tierlistdata/tierlistdata.ts
@@ -2,13 +2,43 @@ import dbCore from "../../../../core/db-core";
 import * as express from "express";
 const router = express.Router({ mergeParams: true });
 import tierlistCore from "../../../../core/tierlist-core";
+import common from "../../../../core/common-core";
 
-// mounted on /api/v1/tierlists/tierlistdata
+/**
+ * @namespace /v1/tierlists/tierlistdata
+ */
 
 const RETURN_LIMIT = 100;
 
-router.get("/", async function (req, res) {
+interface TierlistDataResponse extends FancyQueryBody<TierlistDataDocument> {
+    tierlist: TierlistDocument;
+}
+
+/**
+ * Retrieves up to 100 tierlistdata objects for a given tierlist.
+ * @name GET /v1/tierlists/tierlistdata
+ * @param tierlistID - the ID of the tierlist you wish to request tierlistdata from.
+ * @param game - Alternatively, providing a game and a playtype, this will select the default
+ * tierlist for said game.
+ * @param playtype - See above, necessary if providing game.
+ */
+router.get("/", async (req: KTRequest, res) => {
     let tierlist = null;
+
+    if (!common.IsValidGame(req.query.game)) {
+        return res.status(400).json({
+            success: false,
+            description: `Invalid value for game ${req.query.game}`,
+        });
+    }
+
+    if (!common.IsValidPlaytype(req.query.playtype, req.query.game)) {
+        return res.status(400).json({
+            success: false,
+            description: `Invalid value for playtype ${req.query.playtype} for game ${req.query.game}`,
+        });
+    }
+
     if (req.query.tierlistID) {
         tierlist = await tierlistCore.GetTierlistWithID(req.query.tierlistID);
     } else {
@@ -24,26 +54,18 @@ router.get("/", async function (req, res) {
 
     req.query.tierlistID = tierlist.tierlistID;
 
-    try {
-        let dbRes = await dbCore.FancyDBQuery("tierlistdata", req.query, true, RETURN_LIMIT);
+    let dbRes = await dbCore.FancyDBQuery<TierlistDataDocument>(
+        "tierlistdata",
+        req.query,
+        true,
+        RETURN_LIMIT
+    );
 
-        if (dbRes.body.success) {
-            dbRes.body.body.tierlist = tierlist;
-        }
-
-        return res.status(dbRes.statusCode).json(dbRes.body);
-    } catch (r) {
-        if (r.statusCode && r.body) {
-            return res.status(r.statusCode).json(r.body);
-        } else {
-            console.error(req.originalUrl);
-            console.error(r);
-            return res.status(500).json({
-                success: false,
-                description: "An unknown internal server error has occured.",
-            });
-        }
+    if (dbRes.body.success) {
+        (dbRes.body.body as TierlistDataResponse).tierlist = tierlist;
     }
+
+    return res.status(dbRes.statusCode).json(dbRes.body);
 });
 
 export default router;
