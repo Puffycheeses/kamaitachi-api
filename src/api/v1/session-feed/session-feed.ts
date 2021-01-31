747f8eb492ae2eb3fe1cdba1ad0e2003c1ed7a4e
zkldi <ddhitone@gmail.com>
2021-01-31 21:32:50 +0000

    Convert session-feed to TS

--- src/api/v1/session-feed/session-feed.ts
+++ src/api/v1/session-feed/session-feed.ts
@@ -5,134 +5,153 @@ import db from "../../../db";
 import apiConfig from "../../../apiconfig";
 import sessionCore from "../../../core/session-core";
 
-// Returns data optimised for displaying the session feed.
-// (saves having to make multiple requests.)
-// note that, if this looks strange, it's because it is making optimisations **everywhere**.
+/**
+ * @namespace /v1/session-feed
+ */
+
+interface SessionFeedReturns extends FancyQueryBody<SessionDocument> {
+    highlightedScores?: Record<string, ScoreDocument[]>;
+    users?: PublicUserDocument[];
+    charts?: Record<Game, ChartDocument[]>;
+    songs?: Record<Game, SongDocument[]>;
+}
+
+/**
+ * Returns data optimised for displaying a session feed.
+ * @name GET /v1/session-feed
+ * @note This is incredibly optimised, and as a result is unintuitive code.
+ */
 const MAX_RETURNS = 50;
 router.get("/", async (req: KTRequest, res) => {
-    try {
-        let queryObj = {};
-        queryObj = await sessionCore.HandleCustomUserSelections(req, queryObj);
+    let queryObj = {};
+    queryObj = await sessionCore.HandleCustomUserSelections(req, queryObj);
 
-        let dbRes = await dbCore.FancyDBQuery(
-            "sessions",
-            req.query,
-            true,
-            MAX_RETURNS,
-            undefined,
-            false,
-            queryObj
+    let dbRes = (await dbCore.FancyDBQuery<SessionDocument>(
+        "sessions",
+        req.query,
+        true,
+        MAX_RETURNS,
+        undefined,
+        false,
+        queryObj
+    )) as FancyQueryPseudoResponse<SessionDocument>;
+
+    if (dbRes.body.success) {
+        let users = new Set();
+        let chartIDs: Partial<Record<Game, Set<string>>> = {};
+        let songIDs: Partial<Record<Game, Set<number>>> = {};
+        // sessionID to highlightedScoresMap
+
+        let sessionFeedReturns: SessionFeedReturns = dbRes.body.body;
+
+        sessionFeedReturns.highlightedScores = {};
+        let promiseArr = [];
+
+        for (const session of sessionFeedReturns.items) {
+            users.add(session.userID);
+
+            // @ts-expect-error Monkeypatching scoreCount onto session document.
+            session.scoreCount = session.scores.length;
+
+            let scoreIDs = session.scores.map((e) => e.scoreID);
+
+            let p = db
+                .get("scores")
+                .find(
+                    {
+                        scoreID: { $in: scoreIDs },
+                        highlight: true,
+                    },
+                    {
+                        projection: {
+                            songID: 1,
+                            scoreID: 1,
+                            chartID: 1,
+                            "scoreData.grade": 1,
+                            "scoreData.gradeIndex": 1,
+                            "scoreData.percent": 1,
+                            "scoreData.score": 1,
+                            "scoreData.lamp": 1,
+                            "scoreData.lampIndex": 1,
+                        },
+                    }
+                )
+                // I have absolutely no idea how this eslint rule works but it clearly doesn't
+                // work well for typescript
+                // eslint-disable-next-line no-loop-func
+                .then((rx) => {
+                    if (!rx.length) {
+                        return;
+                    }
+
+                    let r: ScoreDocument[] = rx;
+
+                    sessionFeedReturns.highlightedScores![session.sessionID] = r;
+
+                    if (!chartIDs[session.game]) {
+                        chartIDs[session.game] = new Set();
+                    }
+
+                    if (!songIDs[session.game]) {
+                        songIDs[session.game] = new Set();
+                    }
+
+                    for (const sc of r) {
+                        chartIDs[session.game]!.add(sc.chartID);
+                        songIDs[session.game]!.add(sc.songID);
+                    }
+                });
+
+            promiseArr.push(p);
+
+            // @ts-expect-error I know that these parameters should be optional
+            // but we're doing wacky things here anyway
+            // dont worry about it.
+            delete session.scores; // remove useless data from response.
+        }
+
+        await Promise.all(promiseArr);
+
+        let allSongs: Partial<Record<Game, SongDocument[]>> = {};
+        let allCharts: Partial<Record<Game, ChartDocument[]>> = {};
+
+        // despite the fact that this is a record with key Game
+        // ts is INSISTENT that g should be a string.
+        // it's not, and it shouldnt be, and TS should shut the hell up.
+        for (const g in chartIDs) {
+            let game: Game = g as Game;
+
+            let songs = await db.get(`songs-${game}`).find({
+                id: { $in: [...songIDs[game]!] },
+            });
+
+            let charts = await db.get(`charts-${game}`).find({
+                chartID: { $in: [...chartIDs[game]!] },
+            });
+
+            allSongs[game] = songs;
+            allCharts[game] = charts;
+        }
+
+        sessionFeedReturns.charts = allCharts as Record<Game, ChartDocument[]>;
+        sessionFeedReturns.songs = allSongs as Record<Game, SongDocument[]>;
+
+        let dbUsers = await db.get("users").find(
+            {
+                id: { $in: [...users] },
+            },
+            {
+                projection: apiConfig.REMOVE_PRIVATE_USER_RETURNS,
+            }
         );
 
-        if (dbRes.body.success) {
-            let users = new Set();
-            let chartIDs = new Set();
-            let songIDs = new Set();
-            // sessionID to highlightedScoresMap
+        sessionFeedReturns.users = dbUsers;
 
-            dbRes.body.body.highlightedScores = {};
-            let promiseArr = [];
-
-            for (const session of dbRes.body.body.items) {
-                users.add(session.userID);
-
-                // monkey patch
-                session.scoreCount = session.scores.length;
-
-                let scoreIDs = session.scores.map((e) => e.scoreID);
-
-                let p = db
-                    .get("scores")
-                    .find(
-                        {
-                            scoreID: { $in: scoreIDs },
-                            highlight: true,
-                        },
-                        {
-                            projection: {
-                                songID: 1,
-                                scoreID: 1,
-                                chartID: 1,
-                                "scoreData.grade": 1,
-                                "scoreData.gradeIndex": 1,
-                                "scoreData.percent": 1,
-                                "scoreData.score": 1,
-                                "scoreData.lamp": 1,
-                                "scoreData.lampIndex": 1,
-                            },
-                        }
-                    )
-                    .then((r) => {
-                        if (!r.length) {
-                            return;
-                        }
-
-                        dbRes.body.body.highlightedScores[session.sessionID] = r;
-
-                        if (!chartIDs[session.game]) {
-                            chartIDs[session.game] = new Set();
-                        }
-                        if (!songIDs[session.game]) {
-                            songIDs[session.game] = new Set();
-                        }
-
-                        for (const sc of r) {
-                            chartIDs[session.game].add(sc.chartID);
-                            songIDs[session.game].add(sc.songID);
-                        }
-                    });
-
-                promiseArr.push(p);
-
-                delete session.scores; // remove useless data from response.
-            }
-
-            await Promise.all(promiseArr);
-
-            let allSongs = {};
-            let allCharts = {};
-
-            for (const game in chartIDs) {
-                let songs = await db.get(`songs-${game}`).find({
-                    id: { $in: [...songIDs[game]] },
-                });
-
-                let charts = await db.get(`charts-${game}`).find({
-                    chartID: { $in: [...chartIDs[game]] },
-                });
-
-                allSongs[game] = songs;
-                allCharts[game] = charts;
-            }
-
-            dbRes.body.body.charts = allCharts;
-            dbRes.body.body.songs = allSongs;
-
-            let dbUsers = await db.get("users").find(
-                {
-                    id: { $in: [...users] },
-                },
-                {
-                    projection: apiConfig.REMOVE_PRIVATE_USER_RETURNS,
-                }
-            );
-
-            dbRes.body.body.users = dbUsers;
-        }
-
-        return res.status(dbRes.statusCode).json(dbRes.body);
-    } catch (r) {
-        if (r.statusCode && r.body) {
-            return res.status(r.statusCode).json(r.body);
-        } else {
-            console.error(req.originalUrl);
-            console.error(r);
-            return res.status(500).json({
-                success: false,
-                description: "An unknown internal server error has occured.",
-            });
-        }
+        // assign back
+        dbRes.body.body = sessionFeedReturns;
     }
+
+    return res.status(dbRes.statusCode).json(dbRes.body);
 });
 
 export default router;
