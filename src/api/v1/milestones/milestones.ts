6a32b14efce60a7195df43e142e3bc0dbec31bc7
zkldi <ddhitone@gmail.com>
2021-01-31 14:25:59 +0000

    Completely rewrite milestones.ts

--- src/api/v1/milestones/milestones.ts
+++ src/api/v1/milestones/milestones.ts
@@ -1,42 +1,38 @@
 import * as express from "express";
 import dbCore from "../../../core/db-core";
 const router = express.Router({ mergeParams: true });
-import db from "../../../db";
-import apiConfig from "../../../apiconfig";
-// mounted on /api/v1/milestones
+import userCore from "../../../core/user-core";
+
+/**
+ * @namespace /v1/milestones
+ */
 
 const MAX_RETURNS = 100;
-router.get("/", async (req, res) => {
-    try {
-        let dbRes = await dbCore.FancyDBQuery("milestones", req.query, true, MAX_RETURNS);
 
-        if (dbRes.body.success) {
-            if (req.query.getAssocUsers) {
-                let users = await db.get("users").find(
-                    {
-                        id: { $in: dbRes.body.body.items.map((e) => e.createdBy) },
-                    },
-                    {
-                        projection: apiConfig.REMOVE_PRIVATE_USER_RETURNS,
-                    }
-                );
+interface MilestoneFQReturns extends FancyQueryBody<unknown> {
+    users: PublicUserDocument[];
+}
 
-                dbRes.body.body.users = users;
-            }
-        }
-        return res.status(dbRes.statusCode).json(dbRes.body);
-    } catch (r) {
-        if (r.statusCode && r.body) {
-            return res.status(r.statusCode).json(r.body);
-        } else {
-            console.error(req.originalUrl);
-            console.error(r);
-            return res.status(500).json({
-                success: false,
-                description: "An unknown internal server error has occured.",
-            });
+/**
+ * Performs a FancyQuery on milestones.
+ * @name GET /v1/milestones
+ */
+router.get("/", async (req: KTRequest, res) => {
+    let dbRes = (await dbCore.FancyDBQuery<MilestoneDocument>(
+        "milestones",
+        req.query,
+        true,
+        MAX_RETURNS
+    )) as FancyQueryPseudoResponse<MilestoneDocument>;
+
+    if (dbRes.body.success) {
+        if (req.query.getAssocUsers) {
+            let users = await userCore.GetUsers(dbRes.body.body.items.map((e) => e.createdBy));
+
+            (dbRes.body.body as MilestoneFQReturns).users = users;
         }
     }
+    return res.status(dbRes.statusCode).json(dbRes.body);
 });
 
 import milestoneIDRouter from "./milestoneID/milestoneID";
