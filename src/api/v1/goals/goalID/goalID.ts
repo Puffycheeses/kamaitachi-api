2430752d87d963947a0d4605f81c614bce93f393
zkldi <ddhitone@gmail.com>
2021-01-31 14:25:24 +0000

    Completely rewrite goalID.ts

--- src/api/v1/goals/goalID/goalID.ts
+++ src/api/v1/goals/goalID/goalID.ts
@@ -3,9 +3,11 @@ import goalCore from "../../../../core/goal-core";
 const router = express.Router({ mergeParams: true });
 import db from "../../../../db";
 
-// mounted on /api/v1/goals/goal/:goalID
+/**
+ * @namespace /v1/goals/goal/:goalID
+ */
 
-async function ValidateGoalID(req, res, next) {
+async function ValidateGoalID(req: KTRequest, res: express.Response, next: express.NextFunction) {
     let goal = await db.get("goals").findOne({
         goalID: req.params.goalID,
     });
@@ -24,18 +26,26 @@ async function ValidateGoalID(req, res, next) {
 
 router.use(ValidateGoalID);
 
+/**
+ * Retreives the goal at the given ID.
+ * @name GET /v1/goals/goal/:goalID
+ */
 router.get("/", async (req, res) =>
     res.status(200).json({
         success: true,
-        description: `Successfully found goal ${req.ktchiGoal.title}.`,
+        description: `Successfully found goal ${req.ktchiGoal!.title}.`,
         body: req.ktchiGoal,
     })
 );
 
+/**
+ * Assigns a given goal id to the requesting user.
+ * @name PATCH /v1/goals/goal/:goalID/assign-goal
+ */
 router.patch("/assign-goal", async (req, res) => {
-    let goal = req.ktchiGoal;
+    let goal = req.ktchiGoal as GoalDocument;
     let exists = await db.get("user-goals").findOne({
-        userID: req.apikey.assignedTo,
+        userID: req.apikey!.assignedTo,
         goalID: req.params.goalID,
     });
 
@@ -47,18 +57,22 @@ router.patch("/assign-goal", async (req, res) => {
     }
 
     // else create user-goals obj
-    let ugObj = await goalCore.CreateUserGoal(goal, req.apikey.assignedTo);
+    let ugObj = await goalCore.CreateUserGoal(goal, req.apikey!.assignedTo);
 
     return res.status(201).json({
         success: true,
-        description: `Successfully added goal ${req.ktchiGoal.title}.`,
+        description: `Successfully added goal ${goal.title}.`,
         body: ugObj,
     });
 });
 
+/**
+ * Removes the given goalID from the requesting user.
+ * @name DELETE /v1/goals/goal/:goalID/remove-goal
+ */
 router.delete("/remove-goal", async (req, res) => {
     let exists = await db.get("user-goals").findOne({
-        userID: req.apikey.assignedTo,
+        userID: req.apikey!.assignedTo,
         goalID: req.params.goalID,
     });
 
