fb8b5639928f4cff69f190d6bd56d8a300fc1ea4
zkldi <ddhitone@gmail.com>
2021-01-31 14:26:20 +0000

    Completely rewrite queries.ts

--- src/api/v1/queries/queries.ts
+++ src/api/v1/queries/queries.ts
@@ -1,47 +1,55 @@
 import * as express from "express";
 const router = express.Router({ mergeParams: true });
 import dbCore from "../../../core/db-core";
-import jsum from "jsum";
+import JSum from "jsum";
 import db from "../../../db";
 import apiConfig from "../../../apiconfig";
 
-// mounted on /v1/queries
+/**
+ * @namespace /v1/queries
+ */
 
 // query for queries
 // confusing naming, i guess?
 const RETURN_LIMIT = 100;
-router.get("/query", async (req, res) => {
-    try {
-        let dbr = await dbCore.FancyDBQuery("queries", req.query, true, RETURN_LIMIT);
 
-        if (dbr.body.success) {
-            if (req.query.getAssocData === "true") {
-                let users = await db.get("users").find(
-                    {
-                        id: { $in: dbr.body.body.items.map((e) => e.byUser) },
-                    },
-                    { projection: apiConfig.REMOVE_PRIVATE_USER_RETURNS }
-                );
+interface QueryFQReturns extends FancyQueryBody<unknown> {
+    users: PublicUserDocument[];
+}
 
-                dbr.body.body.users = users;
-            }
-        }
+/**
+ * Performs a query for query documents.
+ * haha - zkldi
+ * @name GET /v1/queries/query
+ */
+router.get("/query", async (req: KTRequest, res) => {
+    let dbr = (await dbCore.FancyDBQuery<QueryDocument>(
+        "queries",
+        req.query,
+        true,
+        RETURN_LIMIT
+    )) as FancyQueryPseudoResponse<QueryDocument>;
 
-        return res.status(dbr.statusCode).json(dbr.body);
-    } catch (r) {
-        if (r.statusCode && r.body) {
-            return res.status(r.statusCode).json(r.body);
-        } else {
-            console.error(req.originalUrl);
-            console.error(r);
-            return res.status(500).json({
-                success: false,
-                description: "An unknown internal server error has occured.",
-            });
+    if (dbr.body.success) {
+        if (req.query.getAssocData) {
+            let users = await db.get("users").find(
+                {
+                    id: { $in: dbr.body.body.items.map((e) => e.byUser) },
+                },
+                { projection: apiConfig.REMOVE_PRIVATE_USER_RETURNS }
+            );
+
+            (dbr.body.body as QueryFQReturns).users = users;
         }
     }
+
+    return res.status(dbr.statusCode).json(dbr.body);
 });
 
+/**
+ * Deletes the query at the given ID.
+ * @name DELETE /v1/queries/remove-query
+ */
 router.delete("/remove-query", async (req, res) => {
     if (!req.body.queryID) {
         return res.status(400).json({
@@ -61,7 +69,7 @@ router.delete("/remove-query", async (req, res) => {
         });
     }
 
-    if (query.byUser !== req.apikey.assignedTo) {
+    if (query.byUser !== req.apikey!.assignedTo) {
         return res.status(401).json({
             success: false,
             description: "This is not your query to delete.",
@@ -90,7 +98,13 @@ const validQueryOptions = [
     "scoreData.percent",
 ];
 
-router.put("/add-query", async (req, res) => {
+/**
+ * Creates a query and adds it to the query db.
+ * @name PUT /v1/queries/add-query
+ * @param name - The name of the query, must be less than 40 chars
+ * @param desc - The description of the query, must be less than 240 chars.
+ */
+router.put("/add-query", async (req: KTRequest, res) => {
     if (!req.body.name) {
         return res.status(400).json({
             success: false,
@@ -123,16 +137,20 @@ router.put("/add-query", async (req, res) => {
         });
     }
 
-    let queryObj = { query: {} };
+    let queryObj: Partial<QueryDocument> = { query: {} };
 
     for (const qOpt of validQueryOptions) {
         if (req.body[qOpt]) {
             let actualKeyName = qOpt.replace(/\./g, "¬");
-            queryObj.query[actualKeyName] = req.body[qOpt];
+            queryObj.query![actualKeyName] = req.body[qOpt];
+        }
+        if (req.body[`${qOpt}-opt`]) {
+            let actualKeyName = qOpt.replace(/\./g, "¬");
+            queryObj.query![`${actualKeyName}-opt`] = req.body[`${actualKeyName}-opt`];
         }
     }
 
-    let queryID = jsum.digest(queryObj.query, "SHA1", "hex");
+    let queryID = JSum.digest(queryObj.query, "SHA1", "hex");
 
     let exists = await db.get("queries").findOne({
         queryID: queryID,
@@ -148,12 +166,12 @@ router.put("/add-query", async (req, res) => {
     queryObj.queryID = queryID;
     queryObj.name = strName;
     queryObj.desc = strDesc;
-    queryObj.byUser = req.apikey.assignedTo;
+    queryObj.byUser = req.apikey!.assignedTo;
     queryObj.timeAdded = Date.now();
     queryObj.timesUsed = 0;
     queryObj.forDatabase = "scores"; // temp, can be expanded in future.
 
-    db.get("queries").insert(queryObj);
+    await db.get("queries").insert(queryObj);
 
     return res.status(201).json({
         success: true,
