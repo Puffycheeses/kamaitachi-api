eb4f5a36823e81f477bf397800a902e5b6b994d0
zkldi <ddhitone@gmail.com>
2021-02-06 15:56:18 +0000

    Remove useless config information from /games/game

--- src/api/v1/games/game/game.ts
+++ src/api/v1/games/game/game.ts
@@ -12,43 +12,20 @@ router.use(middlewares.RequireValidGame);
 
 /**
  * Returns information about that specific game,
- * including config settings, songs, charts and scores.
  * @name GET /v1/games/:game
  */
 router.get("/", async (req, res) => {
     let scoreCount = await db.get("scores").count({ game: req.params.game });
     let songCount = await db.get(`songs-${req.params.game}`).count({});
     let chartCount = await db.get(`charts-${req.params.game}`).count({});
-    let game = req.params.game as Game;
-
-    let gameObj = {
-        game: game,
-        gameHuman: config.gameHuman[game],
-        validPlaytypes: config.validPlaytypes[game],
-        versions: config.gameOrders[game],
-        versionsHuman: config.versionHuman[game],
-        defaultPlaytype: config.defaultPlaytype[game],
-        grades: config.grades[game],
-        gradeBoundaries: config.gradeBoundaries[game],
-        folders: config.folders[game],
-        lamps: config.lamps[game],
-        clearLamp: config.clearLamp[game],
-        chartIndicators: config.gameChartIndicators[game],
-        ratingParameters: config.ratingParameters[game],
-    };
-
-    let gameStats = {
-        scoreCount: scoreCount,
-        songCount: songCount,
-        chartCount: chartCount,
-    };
 
     return res.status(200).json({
         success: true,
         description: `Information retrieved for ${req.params.game}.`,
         body: {
-            gameInfo: gameObj,
-            gameStats: gameStats,
+            scoreCount: scoreCount,
+            songCount: songCount,
+            chartCount: chartCount,
         },
     });
 });
