6356e466f7a3cf0409bebced5fd0f4ad64c1e286
zkldi <ddhitone@gmail.com>
2021-02-06 22:20:21 +0000

    add link-status

--- src/api/v1/users/userID/userID.ts
+++ src/api/v1/users/userID/userID.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 const router = express.Router({ mergeParams: true });
 import middlewares from "../../../../middlewares";
+import db from "../../../../db";
 
 /**
  * @namespace /v1/users/:userID
@@ -19,6 +20,31 @@ router.get("/", async (req, res) =>
         body: req.requestedUser,
     })
 );
+
+/**
+ * Determines whether the user is linked with the given service.
+ * This can only be accessed by the requesting user.
+ * @name GET /v1/users/:userID/link-status
+ */
+router.get("/link-status", middlewares.RequireUserKeyMatch, async (req: KTRequest, res) => {
+    if (!["arc", "flo", "eag"].includes(req.query.service)) {
+        return res.status(400).json({
+            success: false,
+            description: "Invalid service.",
+        });
+    }
+
+    // get the integration info of the user.
+    let secureUser = await db.get("users").findOne({ id: req.user!.id });
+
+    return res.status(200).json({
+        success: true,
+        description: "Retrieved link status.",
+        body: {
+            linkStatus: !!secureUser?.integrations?.[req.query.service]?.authStatus,
+        },
+    });
+});
 
 // mounts
 import friendsRouter from "./friends/friends";
