bd9ef9b76139b5c386d4e792a803899a7c950a29
zkldi <ddhitone@gmail.com>
2021-02-06 16:03:38 +0000

    refactor to userCore in sessions

--- src/api/v1/sessions/sessions.ts
+++ src/api/v1/sessions/sessions.ts
@@ -2,8 +2,7 @@ import * as express from "express";
 import dbCore from "../../../core/db-core";
 import sessionCore from "../../../core/session-core";
 const router = express.Router({ mergeParams: true });
-import db from "../../../db";
-import apiConfig from "../../../apiconfig";
+import userCore from "../../../core/user-core";
 
 /**
  * @namespace /v1/sessions
@@ -24,25 +23,19 @@ router.get("/", async (req: KTRequest, res) => {
 
     queryObj = await sessionCore.HandleCustomUserSelections(req, queryObj);
 
-    let dbRes = (await dbCore.FancyDBQuery<SessionDocument>(
+    let dbRes = await dbCore.NBQuery<SessionDocument>(
         "sessions",
         req.query,
         true,
         MAX_RETURNS,
         undefined,
-        false,
         queryObj
-    )) as FancyQueryPseudoResponse<SessionDocument>;
+    );
 
     if (dbRes.body.success) {
         if (req.query.getAssocData === "true") {
-            (dbRes.body.body as SessionFQResponse).users = await db.get("users").find(
-                {
-                    id: { $in: dbRes.body.body.items.map((e) => e.userID) },
-                },
-                {
-                    projection: apiConfig.REMOVE_PRIVATE_USER_RETURNS,
-                }
+            (dbRes.body.body as SessionFQResponse).users = await userCore.GetUsers(
+                dbRes.body.body.items.map((e) => e.userID)
             );
         }
     }
