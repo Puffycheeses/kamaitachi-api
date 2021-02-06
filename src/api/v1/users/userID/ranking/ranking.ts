4a8abcf3727f445267fb77a3c8544fdc30f65935
zkldi <ddhitone@gmail.com>
2021-02-06 16:07:49 +0000

    refactor to allow defaultPlaytype in ranking.

--- src/api/v1/users/userID/ranking/ranking.ts
+++ src/api/v1/users/userID/ranking/ranking.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 const router = express.Router({ mergeParams: true });
 import db from "../../../../../db";
 import common from "../../../../../core/common-core";
+import config from "../../../../../config/config";
 
 /**
  * @namespace /v1/users/:userID/ranking
@@ -23,33 +24,35 @@ router.get("/", async (req: KTRequest, res) => {
         });
     }
 
-    let playtype = req.query.playtype;
+    let playtype = config.defaultPlaytype[game];
 
-    if (!common.IsValidPlaytype(playtype, game)) {
-        return res.status(400).json({
-            success: false,
-            description: "Invalid or no playtype provided.",
-        });
+    if (req.query.playtype) {
+        if (!common.IsValidPlaytype(playtype, game)) {
+            return res.status(400).json({
+                success: false,
+                description: "Invalid or no playtype provided.",
+            });
+        }
     }
 
     if (user.ratings[game] && user.ratings[game][playtype]) {
         let ranking = await db
             .get("users")
-            .count({ [`ratings.${game}.${playtype}`]: { $gte: user.ratings[game][playtype] } });
+            .count({ [`ratings.${game}.${playtype}`]: { $gt: user.ratings[game][playtype] } });
 
         return res.status(200).json({
             success: true,
             description: "Found users ranking.",
             body: {
-                ranking: ranking,
+                ranking: ranking + 1,
             },
         });
-    } else {
-        return res.status(400).json({
-            success: false,
-            description: "Could not find ranking for user (has user played this game?).",
-        });
     }
+
+    return res.status(400).json({
+        success: false,
+        description: "Could not find ranking for user (has user played this game?).",
+    });
 });
 
 export default router;
