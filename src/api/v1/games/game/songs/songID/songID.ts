3a33b29a29a86d7b7aebd140acf695868232a99b
zkldi <ddhitone@gmail.com>
2021-02-06 15:57:13 +0000

    remove /charts from songID

--- src/api/v1/games/game/songs/songID/songID.ts
+++ src/api/v1/games/game/songs/songID/songID.ts
@@ -1,7 +1,6 @@
 import * as express from "express";
 const router = express.Router({ mergeParams: true });
 import middlewares from "../../../../../../middlewares";
-import dbCore from "../../../../../../core/db-core";
 
 /**
  * @namespace /v1/games/:game/songs/:songID
@@ -23,34 +22,6 @@ router.get("/", async (req, res) => {
             item: song,
         },
     });
-});
-
-const CHART_RET_LIMIT = 100;
-
-interface ChartsReturn extends FancyQueryBody<ChartDocument> {
-    song: SongDocument;
-}
-
-/**
- * Returns the charts that belong to the song at the given ID.
- * @name GET /v1/games/:game/songs/:songID/charts
- */
-router.get("/charts", async (req: KTRequest, res) => {
-    req.query.id = req.params.songID;
-
-    let dbRes = await dbCore.FancyDBQuery(
-        `charts-${req.params.game}` as ValidDatabases,
-        req.query,
-        true,
-        CHART_RET_LIMIT,
-        "charts"
-    );
-
-    if (dbRes.body.success) {
-        (dbRes.body.body as ChartsReturn).song = req.song!;
-    }
-
-    return res.status(dbRes.statusCode).json(dbRes.body);
 });
 
 export default router;
