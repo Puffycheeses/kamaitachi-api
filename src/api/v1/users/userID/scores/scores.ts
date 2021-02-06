e8108907d9a32dfb2b99c3fac6f414eb83b21ba5
zkldi <ddhitone@gmail.com>
2021-02-06 16:08:02 +0000

    move /best to :userID/best

--- src/api/v1/users/userID/scores/scores.ts
+++ src/api/v1/users/userID/scores/scores.ts
@@ -3,6 +3,7 @@ const router = express.Router({ mergeParams: true });
 import dbCore from "../../../../../core/db-core";
 import db from "../../../../../db";
 import common from "../../../../../core/common-core";
+import scoreCore from "../../../../../core/score-core";
 
 /**
  * @namespace /v1/users/:userID/scores
@@ -17,9 +18,87 @@ router.get("/count", async (req: KTRequest, res) => {
 
     req.query.userID = user.id.toString();
 
-    let dbRes = await dbCore.FancyDBQuery("scores", req.query, false, undefined, undefined, true);
+    let dbRes = await dbCore.NBCount("scores", req.query, false, undefined, undefined);
 
     return res.status(dbRes.statusCode).json(dbRes.body);
+});
+
+/**
+ * Retrieves a users' best 100 scores sorted on calculatedData.rating.
+ * @name GET /v1/users/:userID/scores/best
+ * @param game
+ * @param playtype
+ */
+router.get("/best", async (req: KTRequest, res) => {
+    let game = req.query.game;
+
+    if (!common.IsValidGame(game)) {
+        return res.status(400).json({
+            success: false,
+            description: "This game is not supported, or one was not provided.",
+        });
+    }
+
+    if (!common.IsValidPlaytype(req.query.playtype, game)) {
+        return res.status(400).json({
+            success: false,
+            description: "This playtype is not supported, or one was not provided.",
+        });
+    }
+
+    let startPoint = common.AssertPositiveInteger(req.query.start, 0);
+
+    let limit = common.AssertPositiveInteger(req.query.limit, 100, true);
+
+    // else if we get here we're all good
+
+    let bestScores = (await db.get("scores").find(
+        {
+            userID: parseInt(req.params.userID),
+            game: game,
+            "scoreData.playtype": req.query.playtype,
+            isScorePB: true,
+        },
+        {
+            sort: { "calculatedData.rating": -1 },
+            limit: limit,
+            skip: startPoint,
+        }
+    )) as ScoreDocument[];
+
+    if (req.query.autocoerce !== "false") {
+        bestScores = await scoreCore.AutoCoerce(bestScores);
+    }
+
+    if (bestScores.length === 0) {
+        return res.status(200).json({
+            success: true,
+            description: "This user has no scores.",
+            body: {
+                scores: [],
+                songs: [],
+                charts: [],
+            },
+        });
+    }
+
+    let songs = await db
+        .get(`songs-${game}`)
+        .find({ id: { $in: bestScores.map((e) => e.songID) } });
+
+    let charts = await db.get(`charts-${game}`).find({
+        chartID: { $in: bestScores.map((e) => e.chartID) },
+    });
+
+    return res.status(200).json({
+        success: true,
+        description: `Found ${bestScores.length} scores.`,
+        body: {
+            scores: bestScores,
+            songs: songs,
+            charts: charts,
+        },
+    });
 });
 
 interface PartialTimestampScore {
@@ -56,17 +135,17 @@ router.get("/heatmap", async (req: KTRequest, res) => {
         }
 
         queryObj.game = req.query.game;
-    }
 
-    if (req.query.playtype) {
-        if (!req.query.game) {
-            return res.status(400).json({
-                success: false,
-                description: "Cannot specify playtype without specifying game.",
-            });
+        if (req.query.playtype) {
+            if (!common.IsValidPlaytype(req.query.playtype, req.query.game)) {
+                return res.status(400).json({
+                    success: false,
+                    description: `Invalid playtype ${req.query.playtype}.`,
+                });
+            }
+
+            queryObj["scoreData.playtype"] = req.query.playtype;
         }
-
-        queryObj["scoreData.playtype"] = req.query.playtype;
     }
 
     let timeData: PartialTimestampScore[] = await db.get("scores").find(queryObj, {
